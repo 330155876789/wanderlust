95523a642b50e304ccb9aada10408654dc4c8ecf
vikram kathole <vikramkathole2225@gmail.com>
2025-05-31 11:39:30 +0530

    new files added

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const Listing = require('./model/listing');
 const data = require('./init/data');
 const methodeOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const  ExpressError = require('./utils/expressError');
+const asyncWrap = require('./utils/asyncwrap');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -25,109 +27,109 @@ main().then(() => {
   console.error('MongoDB connection error:', err);
 });
 
-async function main(){
+async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
 
-
 // Routes
 app.get('/', (req, res) => {
- res.send('Welcome to the Wanderlust API');
+  res.send('Welcome to the Wanderlust API');
 });
 
-app.get('/listings', async (req, res) => {
-  try {
-    const listings = await Listing.find();
-    res.render('listings/index', { listings });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
+app.get('/listings', asyncWrap(async (req, res) => {
+  const listings = await Listing.find();
+  res.render('listings/index', { listings });
+}));
+
 
 // create route
-// This should come first
 app.get("/listings/new", (req, res) => {
-    res.render("listings/new"); // or however you render your form
-});
-
-app.post('/listings', async (req, res) => {
-
-const newListing = new Listing({
-  title: req.body.title,
-  description: req.body.description,
-  price: req.body.price,
-  location: req.body.location,
-  country: req.body.country,
-  image: {
-    url: req.body.image.url
+  let { price } = req.query;
+  if (price) {
+    price = parseFloat(price);
+    if (isNaN(price) || price < 0) {
+      return res.status(400).send('Invalid price');
+    }
   }
+  res.render("listings/new", { price }); // Pass the price to the form
 });
-//  console.log("Received form data:", req.body);
-//   console.log("New listing object:", newListing);
-   await newListing.save();
-  //  console.log(newListing);
-   res.redirect(`/listings/${newListing._id}`);
-});
+
+app.post('/listings', asyncWrap(async (req, res) => {
+
+  const newListing = new Listing({
+    title: req.body.title,
+    description: req.body.description,
+    price: req.body.price,
+    location: req.body.location,
+    country: req.body.country,
+    image: {
+      url: req.body.image.url
+    }
+  });
+
+  await newListing.save();
+  res.redirect(`/listings/${newListing._id}`);
+}));
 
 // show route
-app.get('/listings/:id', async (req, res) => {
-   const { id } = req.params;
-   const listing = await Listing.findById(id);
-   if (!listing) {
-     return res.status(404).send('Listing not found');
-   }
+app.get('/listings/:id', asyncWrap(async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    return next(new ExpressError('Listing not found', 404));
+  }
   //  console.log(id);
-   res.render('listings/show', { listing });
-});
+  res.render('listings/show', { listing });
+}));
 
 
 // update route
-app.get("/listings/:id/edit",async (req,res)=>{
-  const {id} = req.params;
+app.get("/listings/:id/edit", asyncWrap(async (req, res) => {
+  const { id } = req.params;
   const listing = await Listing.findById(id);
   // console.log(listing);
   if (!listing) {
     return res.status(404).send('Listing not found');
   }
   res.render('listings/edit', { listing });
-});
+}));
 
-app.patch('/listings/:id', async (req , res) => {
+app.patch('/listings/:id', asyncWrap(async (req, res) => {
   const { id } = req.params;
   const { title, description, price } = req.body;
-  try {
-    const listing = await Listing.findByIdAndUpdate(id, { title, description, price }, { new: true });
-    console.log(listing);
-    if (!listing) {
-      return res.status(404).send('Listing not found');
-    }
-    res.redirect(`/listings/${listing._id}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  const listing = await Listing.findByIdAndUpdate(id, { title, description, price }, { new: true });
+  if (!listing) {
+    return res.status(404).send('Listing not found');
   }
-});
+  res.redirect(`/listings/${listing._id}`);
+}));
 
 // delete route
-app.delete('/listings/:id/delete', async (req, res) => {
+app.delete('/listings/:id/delete', asyncWrap(async (req, res) => {
   const { id } = req.params;
-  try {
-    const listing = await Listing.findByIdAndDelete(id);
-    if (!listing) {
-      return res.status(404).send('Listing not found');
-    }
-    res.redirect('/listings');
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  const listing = await Listing.findByIdAndDelete(id);
+  if (!listing) {
+    return res.status(404).send('Listing not found');
   }
+  res.redirect('/listings');
+}));
+
+// Error handling middleware
+
+app.use((req, res, next) => {
+  const err = new ExpressError('Page Not Found', 404);
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = 'Something went wrong' } = err;
+  res.status(statusCode).send(message);
 });
 
 
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
-}); 
+});
 
